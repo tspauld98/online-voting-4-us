18a0630964396f647a4023e7cc1ffeec9806ae0c
Jiaxin Yuan <jiaxin_yuan@intuit.com>
2021-07-01 13:37:51 -0700

    add reducer

--- src/actions/election-tool.js
+++ src/actions/election-tool.js
@@ -1,5 +1,5 @@
 export const APPEND_ELECTION_ACTION = "APPEND_ELECTION";
-export const VALIDATE_USER_INFO_ACTION = "VALIDATE_USER_INFO";
+
 
 export const createAppendElectionAction = (election) => ({
     type: APPEND_ELECTION_ACTION,
