a666dc4e09e187e1b1f16f9968fa28edbd7cde9b
Tim Spaulding <tim_spaulding@intuit.com>
2021-07-01 12:32:00 -0700

    Added initial registrations reducer

---
+++ src/reducers/registration-tool.js
@@ -0,0 +1,8 @@
+export const registrationsReducer = (registrations = [], action) => {
+  switch (action.type) {
+    case '':
+      break;
+      default:
+        return registrations;
+  }
+}
--- src/reducers/votingAppCombine.js
+++ src/reducers/votingAppCombine.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { electionsReducer } from "./election-tool";
+import { registrationsReducer } from "./registration-tool";
 
 export const votingAppCombineReducer = combineReducers({
-    elections: electionsReducer
+    registrations: registrationsReducer,
+    elections: electionsReducer,
 });
