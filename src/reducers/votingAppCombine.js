e5453500b5a683be2dbbc76f22abe6cb0bbef0ed
Alex Pilipenko <alex_pilipenko@intuit.com>
2021-07-02 02:08:20 -0400

    Added missing reducer

--- src/reducers/votingAppCombine.js
+++ src/reducers/votingAppCombine.js
@@ -1,20 +1,29 @@
 import { combineReducers } from "redux";
 import {
-    electionResultsReducer,
-    electionsReducer,
-    propositionsReducer,
-  } from "./election-tool";
-import { registrationsReducer, viewSwitchReducer, voterEditReducer } from "./registration-tool";
-import { validationReducer, errorMessageReducer, electionAlreadyVotedReducer } from "./votes-tool";
+  electionResultsReducer,
+  electionsReducer,
+  propositionsReducer,
+} from "./election-tool";
+import {
+  registrationsReducer,
+  viewSwitchReducer,
+  voterEditReducer,
+} from "./registration-tool";
+import {
+  validationReducer,
+  errorMessageReducer,
+  electionAlreadyVotedReducer,
+} from "./votes-tool";
 
 export const votingAppCombineReducer = combineReducers({
-    voters: registrationsReducer,
-    viewSwitch: viewSwitchReducer,
-    voterEdit: voterEditReducer,
-    userId: validationReducer,
-    errorMessage: errorMessageReducer,
-    alreadyVotedFlag: electionAlreadyVotedReducer,
-    elections: electionsReducer,
-    propositions: propositionsReducer,
-    electionResults: electionResultsReducer,
+  voters: registrationsReducer,
+  viewSwitch: viewSwitchReducer,
+  voterEdit: voterEditReducer,
+  userId: validationReducer,
+  errorMessage: errorMessageReducer,
+  alreadyVotedFlag: electionAlreadyVotedReducer,
+  elections: electionsReducer,
+  propositions: propositionsReducer,
+  electionResults: electionResultsReducer,
+  selectedBallot: electionResultsReducer,
 });
