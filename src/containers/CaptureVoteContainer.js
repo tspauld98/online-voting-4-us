455fb62622d855fd4f62a4675e78f6ac6d6e411d
Alex Pilipenko <alex_pilipenko@intuit.com>
2021-07-01 23:03:48 -0400

    Selecting ballot

--- src/containers/CaptureVoteContainer.js
+++ src/containers/CaptureVoteContainer.js
@@ -1,25 +1,44 @@
-import { useMemo } from 'react';
-import { bindActionCreators } from 'redux';
-import { useDispatch, useSelector } from 'react-redux';
-import { UserValidationForm } from '../components/UserValidationForm';
-import {createValidateUserInfoAction} from '../actions/votes-tool.js';
-import { Ballot } from '../components/Ballot';
+import { useMemo, useEffect } from "react";
+import { useParams } from "react-router";
+import { bindActionCreators } from "redux";
+import { useDispatch, useSelector } from "react-redux";
+import { UserValidationForm } from "../components/UserValidationForm";
+import { createValidateUserInfoAction } from "../actions/votes-tool.js";
+import { loadElection } from "../actions/election-tool";
+import { Ballot } from "../components/Ballot";
 
 export const CaptureVoteContainer = () => {
+  const { ballotId } = useParams();
+  const selectedBallot = useSelector((state) => state.selectedBallot);
+  const validation = useSelector((state) => state.validation);
 
-    const validation = useSelector(state => state.validation);
-  
-    const dispatch = useDispatch();
-  
-    const validationActions = useMemo(() => bindActionCreators({
-      onValidateUserInfo : createValidateUserInfoAction
-    }, dispatch), [dispatch]);
+  const dispatch = useDispatch();
 
-    return (
+  const actions = useMemo(
+    () =>
+      bindActionCreators(
+        {
+          loadElection: loadElection,
+          onValidateUserInfo: createValidateUserInfoAction,
+        },
+        dispatch
+      ),
+    [dispatch]
+  );
+
+  useEffect(() => {
+    actions.loadElection(ballotId);
+  }, [actions, ballotId]);
+
+  return (
     <>
-        <UserValidationForm validation={validation} {...validationActions}/>
-        { validation && <Ballot /> }
-        { !validation && <h1> this ID is invalid </h1> }
+      <UserValidationForm
+        validation={validation}
+        onValidateUserInfo={actions.onValidateUserInfo}
+        selectedBallot={selectedBallot}
+      />
+      {validation && <Ballot selectedBallot={selectedBallot} />}
+      {!validation && <h1> this ID is invalid </h1>}
     </>
-    );
-}
+  );
+};
--- src/reducers/votingAppCombine.js
+++ src/reducers/votingAppCombine.js
@@ -1,14 +1,15 @@
 import { combineReducers } from "redux";
-import { electionsReducer,
+import {
+  electionsReducer,
   electionResultsReducer,
-  propositionsReducer, 
+  propositionsReducer,
 } from "./election-tool";
 import { validationReducer } from "./votes-tool";
 
-
 export const votingAppCombineReducer = combineReducers({
-    validation: validationReducer,
-    elections: electionsReducer,
-    propositions: propositionsReducer,
-    electionResults: electionResultsReducer,
+  validation: validationReducer,
+  elections: electionsReducer,
+  propositions: propositionsReducer,
+  electionResults: electionResultsReducer,
+  selectedBallot: electionResultsReducer,
 });
